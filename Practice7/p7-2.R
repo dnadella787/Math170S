25f9854967944cc852ec2b9bb332bfd7e0dbd752
Dhanush Nadella <dnadella787@gmail.com>
2022-02-01 03:24:21 -0800

    p7-2 done

---
+++ Practice7/p7-2.R
@@ -0,0 +1,26 @@
+
+
+
+
+a = c(481, 537, 513, 583, 453, 510, 570, 500, 457, 555, 
+      618, 327, 350, 643, 499, 421, 505, 637, 599, 392)
+
+abar = mean(a)
+s = sd(a)
+n = length(a)
+alpha = 0.02 
+talpha = qt(1-alpha/2, n-1)
+epsilon = talpha * s / sqrt(n)
+
+sprintf("a_bar : %.9f", abar)
+sprintf("s : %.9f", s)
+sprintf("t_alpha : %.9f", talpha)
+sprintf("epsilon : %.9f", epsilon)
+sprintf("[abar-epsilon, abar+epsilon] : [%.9f, %.9f]", abar - epsilon, abar + epsilon)
+
+
+alpha = 0.005 
+talpha = qt(1-alpha, n-1)
+epsilon = talpha * s / sqrt(n)
+
+sprintf("abar-epsilon : %.9f", abar-epsilon)
--- Practice7/p7.R
+++ Practice7/p7.R
@@ -1,6 +1,3 @@
-
-
-
 x = c(13.0, 18.5, 16.4, 14.8, 19.4, 17.3, 23.2, 24.9,
       20.8, 19.3, 18.8, 23.1, 15.2, 19.9, 19.1, 18.1, 
       25.1, 16.8, 20.4, 17.4, 25.2, 23.1, 15.3, 19.4, 
@@ -15,7 +12,8 @@ epsilon = zalpha * (s/sqrt(length(x)))
 
 sprintf("x_bar : %.9f", xbar)
 sprintf("s : %.9f", s)
-sprintf("z_alpha : %.9f\n", zalpha)
+sprintf("z_alpha : %.9f", zalpha)
 sprintf("epsilon : %.9f", epsilon)
 sprintf("[xbar-epsilon, xbar+epsilon] : [%.9f, %.9f]", xbar - epsilon, xbar + epsilon)
+
 
